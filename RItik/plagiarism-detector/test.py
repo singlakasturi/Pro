22ff449709f90bbcbd65ddee69796103dcbedafb
ARVIND <arvindpuri1492@gmail.com>
2025-04-11 04:08:13 +0530

    hhd

--- RItik/plagiarism-detector/test.py
+++
@@ -1,9 +0,0 @@
-from detectors.ast_detector import ASTDetector
-
-# Test with simple known code
-detector = ASTDetector()
-code1 = "def add(a,b): return a+b"
-code2 = "def sum(x,y): return x+y"
-
-similarity = detector.compare(code1, code2)
-print(f"Test similarity: {similarity:.1f}%")  # Should show ~95%
