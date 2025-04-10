5e55a00da4589ab60386f1a442fb621040b71385
sonali072 <sonalisingla072@gmail.com>
2025-04-11 04:03:28 +0530

    init Commit

--- Pro/src/main/java/HackMol/Pro/controller/SubmissionsController.java
+++ Pro/src/main/java/HackMol/Pro/controller/SubmissionsController.java
@@ -1,6 +1,5 @@
 package HackMol.Pro.controller;
 
-import HackMol.Pro.dto.ContestDTO;
 import HackMol.Pro.dto.SubmissionDTO;
 import HackMol.Pro.services.ResultsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,39 +8,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/contests/{contestId}/questions/{questionId}")
 @CrossOrigin
 public class SubmissionsController {
 
-    private final ResultsService resultsService;
+    private ResultsService resultsService;
 
     @Autowired
     public SubmissionsController(ResultsService resultsService) {
         this.resultsService = resultsService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<SubmissionDTO>> getSubmissionsByQuestionId(
-            @PathVariable String contestId,
-            @PathVariable Integer questionId) {
-
+    @GetMapping()
+    public ResponseEntity<List<SubmissionDTO>> getSubmissionsByQuestionId(@PathVariable String contestId, @PathVariable Integer questionId) {
         List<SubmissionDTO> submissions = resultsService.getSubmissionsByQuestionId(questionId);
         return ResponseEntity.ok(submissions);
     }
 
     @GetMapping("/solution/{userName}")
-    public ResponseEntity<SubmissionDTO> getSubmissionByUserNameAndQuestionId(
-            @PathVariable String contestId,
-            @PathVariable Integer questionId,
-            @PathVariable String userName) {
-
+    public ResponseEntity<SubmissionDTO> getSubmissionByUserNameAndQuestionId(@PathVariable String contestId, @PathVariable Integer questionId, @PathVariable String userName) {
         SubmissionDTO submission = resultsService.getSubmissionByUserNameAndQuestionId(questionId, userName);
-
-        if (submission != null) {
-                return ResponseEntity.ok(submission);
-            } else {
-                return ResponseEntity.status(204).build();
-        }
+        return ResponseEntity.ok(submission);
     }
+
 }
