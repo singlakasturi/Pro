06faa51701dacfd185da83126f53dff66d0b820e
oneeyedkingx10007 <oneeyedkingx10007@gmail.com>
2025-04-10 16:14:59 +0530

    frontend

---
+++ similarity/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="Main">
+
+      </section>
+      <section className="why">
+
+      </section>
+      <section className="mission">
+
+      </section>
+      <section className="about">
+
+      </section>
+      <section className="faq">
+
+      </section>
+      <section className="contact">
+        
+      </section>
+    </>
+  );
+}
+
+export default App;
